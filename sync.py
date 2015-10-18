c01169da7bd4a894239154ac51cd8eb822706dae
Walter Moreira <walter@waltermoreira.net>
2015-10-18 17:48:03 -0500

    Handle direct and indirect mode

--- sync.py
+++ sync.py
@@ -12,12 +12,19 @@ def set_identity(name, email):
         'git config --global user.email "{}"'.format(email).split())
 
 
+def mode():
+    output = subprocess.check_output('git annx info'.split()).decode()
+    return output.splitlines()[0].split(':')[1].strip()
+
+
 def _do_sync():
     os.chdir('/data')
     subprocess.check_call(
         'git annex add .'.split())
-    subprocess.call(
-        'git commit -a -m'.split() + ['"from {}"'.format(os.uname().nodename)])
+    if mode() == 'indirect':
+        subprocess.call(
+            'git commit -a -m'.split() +
+            ['"from {}"'.format(os.uname().nodename)])
     subprocess.check_call('git annex sync'.split())
 
 
